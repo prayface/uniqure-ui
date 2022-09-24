cbfbbdfea969ae9bb70b3bf622036bf28a3c0465
prayface <prayface@163.com>
2022-09-24 17:46:30 +0800

    更新icon图标, icon增加点击事件

--- packages/components/icon/src/icon.vue
+++ packages/components/icon/src/icon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg class="ui-icon" aria-hidden="true">
+    <svg class="ui-icon" aria-hidden="true" @click="$emit('click', $event)">
         <use :xlink:href="icon"></use>
     </svg>
 </template>

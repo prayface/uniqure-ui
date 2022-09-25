90add323818e7bb9be3b84f2810abad3e0b8649d
prayface <prayface@163.com>
2022-09-25 10:55:01 +0800

    loading组件制作完成

---
+++ packages/components/loading/src/loading.vue
@@ -0,0 +1,27 @@
+<template>
+    <transition name="fade">
+        <div class="ui-loading ui-overlay" v-show="show">
+            <div class="ui-loading-container">
+                <ui-icon class="ui-loading-icon" :name="icon" />
+                <div class="ui-loading-context">{{ context }}</div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script lang="ts">
+    import { defineComponent, inject } from "vue"
+    import { UiIcon } from "@uniqure-ui/components/icon"
+    import "@uniqure-ui/assets/less/components/loading.less"
+
+    export default defineComponent({
+        components: { UiIcon },
+        setup() {
+            const icon = inject("icon")
+            const show = inject("show")
+            const context = inject("context")
+
+            return { icon, show, context }
+        }
+    })
+</script>
---
+++ src/views/components/Loading.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="components-container">
+        <div class="container-title">
+            <div class="container-title-content">Loading</div>
+            <div class="container-title-infos">
+                <div class="container-title-info">网站视觉设计图出稿软件为: sktech</div>
+                <div class="container-title-info">使用文字为: 思源黑体</div>
+            </div>
+        </div>
+
+        <section class="section-container">
+            <div class="section-title">Loading:</div>
+            <div class="section-content">
+                <ui-button @click="open">启动按钮</ui-button>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue"
+    import { $loading } from "@uniqure-ui/uniqure-ui/index"
+
+    export default defineComponent({
+        setup() {
+            const open = () => {
+                $loading.open({ context: "测试文本" })
+
+                setTimeout(() => {
+                    $loading.clone()
+                }, 2000)
+            }
+
+            return { open }
+        }
+    })
+</script>
+
+<style lang="less" scoped>
+    @import url("@/assets/less/components/mixins.less");
+</style>
